3faad1cfa3caaa32e5a861e756ec8f4b7bffcdde
Christian Achatz <christian.achatz@adventure-php-framework.org>
2008-09-19 11:04:43 +0000

    Next status of development.

---
+++ apps/sites/apfdocupage/pres/documentcontroller/perspectives/content/sitemap_controller.php
@@ -0,0 +1,76 @@
+<?php
+   import('modules::fulltextsearch::biz','fulltextsearchManager');
+   import('tools::link','linkHandler');
+   import('sites::apfdocupage::biz','APFModel');
+
+
+   /**
+   *  @package sites::apfdocupage::pres::documentcontroller::perspectives::content
+   *  @class sitemap_controller
+   *
+   *  Implements the sitemap document controller.
+   *
+   *  @author Christian Achatz
+   *  @version
+   *  Version 0.1, 19.09.2008<br />
+   */
+   class sitemap_controller extends baseController
+   {
+
+
+      function sitemap_controller(){
+      }
+
+
+      /**
+      *  @public
+      *
+      *  Displays the sitemap list.
+      *
+      *  @author Christian Achatz
+      *  @version
+      *  Version 0.1, 24.03.2008<br />
+      *  Version 0.2, 31.08.2008 (Removed server quick hack)<br />
+      *  Version 0.3, 31.08.2008 (Changed link generation)<br />
+      *  Version 0.4, 19.09.2008 (Moved to sites namespace due to the strong dependency to the APFModel)<br />
+      */
+      function transformContent(){
+
+         // get model
+         $Model = &Singleton::getInstance('APFModel');
+
+         // get fulltextsearch manager
+         $M = &$this->__getServiceObject('modules::fulltextsearch::biz','fulltextsearchManager');
+
+         // load pages
+         $Pages = $M->loadPages();
+
+         // define current page indicator
+         $Language = $Model->getAttribute('page.language');
+         $PageIndicators = $Model->getAttribute('page.indicator');
+         $CurrentPageIndicator = $PageIndicators[$Language];
+
+         // create page link output
+         $count = count($Pages);
+         $Buffer = (string)'';
+         $Template__Page = &$this->__getTemplate('Page');
+
+         for($i = 0; $i < $count; $i++){
+
+            $Template__Page->setPlaceHolder('Title',$Pages[$i]->get('Title'));
+            $Template__Page->setPlaceHolder('LastMod',$Pages[$i]->get('LastMod'));
+            $Template__Page->setPlaceHolder('Link',linkHandler::generateLink('',array($CurrentPageIndicator => $Pages[$i]->get('Name'))));
+            $Buffer .= $Template__Page->transformTemplate();
+
+          // end for
+         }
+
+         // display list
+         $this->setPlaceHolder('PageList',$Buffer);
+
+       // end function
+      }
+
+    // end class
+   }
+?>
