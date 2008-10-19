20e3b4998e9be2f902282645de2fad48e9949081
Christian Achatz <christian.achatz@adventure-php-framework.org>
2008-10-19 12:03:41 +0000

    Next step of development.

---
+++ de/index.php
@@ -0,0 +1,54 @@
+<?php
+   // set error messages to text only
+   ini_set('html_errors','off');
+
+   // include the page controller file (this is necessary to include the APF core layer)
+   include_once('./apps/core/pagecontroller/pagecontroller.php');
+
+   // get registry to adapt the standard values
+   $Reg = &Singleton::getInstance('Registry');
+   $Reg->register('apf::core','URLBasePath','http://apfdocupage/');
+
+   // define environment for the sitemap
+   $Reg->register('sites::apfdocupage','sitemap.env','dev');
+
+   // define forum base url for the home page's forum info box
+   $Reg->register('sites::apfdocupage','ForumBaseURL','http://forum.adventure-php-framework.org');
+
+   // define the release location
+   $Reg->register('sites::apfdocupage','Releases.LocalDir','D:/Entwicklung/Dokumentation/Build/RELEASES');
+   $Reg->register('sites::apfdocupage','Releases.BaseURL','http://dev.adventure-php-framework.org/frontend/media/releases');
+
+   // import front controller
+   import('core::frontcontroller','Frontcontroller');
+
+   // create the front controller instance
+   $fC = &Singleton::getInstance('Frontcontroller');
+
+   // set current context and language
+   $fC->set('Context','sites::apfdocupage');
+   if(isset($_REQUEST['lang'])){
+      $fC->set('Language',$_REQUEST['lang']);
+    // end if
+   }
+   else{
+      $fC->set('Language','de');
+    // end else
+   }
+
+   // register the setModel action
+   $fC->registerAction('sites::apfdocupage::biz','setModel');
+
+   // register the stat action
+   $fC->registerAction('sites::apfdocupage::biz','Stat');
+
+   // start the front controller
+   $fC->start('sites::apfdocupage::pres::templates','apfdocupage');
+
+   // display benchmark report, if benchmarkreport=true is present in the URL
+   if(isset($_REQUEST['benchmarkreport']) && $_REQUEST['benchmarkreport'] == 'true'){
+      $T = &Singleton::getInstance('benchmarkTimer');
+      echo $T->createReport();
+    // end if
+   }
+?>
