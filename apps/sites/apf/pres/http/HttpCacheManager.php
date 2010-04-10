c8144cfd2c5703ea891ee6c44fb5254dfba41d32
Christian Achatz <christian.achatz@adventure-php-framework.org>
2010-04-10 07:47:17 +0000

    Reduced HTML caching to 2 days.

--- apps/sites/apf/pres/http/HttpCacheManager.php
+++ apps/sites/apf/pres/http/HttpCacheManager.php
@@ -11,6 +11,7 @@
     * @author Christian Achatz
     * @version
     * Version 0.1, 23.12.2009<br />
+    * Version 0.2, 10.04.2010 (Reduced HTML caching to 2 days)<br />
     */
    final class HttpCacheManager {
 
@@ -19,28 +20,33 @@
        */
       private static $WEEK_IN_SECS = 604800;
 
+      /**
+       * @var int Two days in seconds.
+       */
+      private static $TWO_DAYS_IN_SECS = 86400;
+
       public static function sendHtmlCacheHeaders(){
          HeaderManager::send('Content-Type: text/html; charset=utf-8');
-         self::sendCacheHeaders();
+         self::sendCacheHeaders(self::$TWO_DAYS_IN_SECS);
       }
 
       public static function sendCssCacheHeaders(){
          HeaderManager::send('Content-Type: text/css; charset=utf-8');
-         self::sendCacheHeaders();
+         self::sendCacheHeaders(self::$WEEK_IN_SECS);
       }
 
       public static function sendJsCacheHeaders(){
          HeaderManager::send('Content-Type: text/javascript; charset=utf-8');
-         self::sendCacheHeaders();
+         self::sendCacheHeaders(self::$WEEK_IN_SECS);
       }
 
-      private static function sendCacheHeaders(){
-         $expires = time() + self::$WEEK_IN_SECS;
+      private static function sendCacheHeaders($expiresTime){
+         $expires = time() + $expiresTime;
          $expiresDate = date('r',$expires);
          $lastModified = date('r',time());
          HeaderManager::send('Expires: '.$expiresDate);
          HeaderManager::send('Last-Modified: '.$lastModified);
-         HeaderManager::send('Cache-Control: public; max-age='.self::$WEEK_IN_SECS);
+         HeaderManager::send('Cache-Control: public; max-age='.$expiresTime);
       }
       
     // end class
