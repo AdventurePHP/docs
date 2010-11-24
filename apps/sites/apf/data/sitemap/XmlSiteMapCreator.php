7a68db55d4c52240d7f0f17fa9410c7d562232a4
Christian Achatz <christian.achatz@adventure-php-framework.org>
2010-11-24 23:00:25 +0000

    Adapted the sitemap creation process to exclude the error pages and generated a new sitemap.

--- apps/sites/apf/data/sitemap/XmlSiteMapCreator.php
+++ apps/sites/apf/data/sitemap/XmlSiteMapCreator.php
@@ -19,11 +19,13 @@
          $buffer = (string)'<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
          $buffer .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'.PHP_EOL;
          while($data = $sql->fetchData($result)){
-            $buffer .= '   <url>'.PHP_EOL;
-            $buffer .= '      <loc>'.$this->baseUrl.$urlMan->generateLink($data['PageID'],$data['Language']).'</loc>'.PHP_EOL;
-            $buffer .= '      <lastmod>'.date('c',strtotime($data['ModificationTimestamp'])).'</lastmod>'.PHP_EOL;
-            $buffer .= '      <changefreq>weekly</changefreq>'.PHP_EOL;
-            $buffer .= '   </url>'.PHP_EOL;
+            if($data['PageID'] != '404'){
+               $buffer .= '   <url>'.PHP_EOL;
+               $buffer .= '      <loc>'.$this->baseUrl.$urlMan->generateLink($data['PageID'],$data['Language']).'</loc>'.PHP_EOL;
+               $buffer .= '      <lastmod>'.date('c',strtotime($data['ModificationTimestamp'])).'</lastmod>'.PHP_EOL;
+               $buffer .= '      <changefreq>weekly</changefreq>'.PHP_EOL;
+               $buffer .= '   </url>'.PHP_EOL;
+            }
          }
 
          $buffer .= '</urlset>'.PHP_EOL;
