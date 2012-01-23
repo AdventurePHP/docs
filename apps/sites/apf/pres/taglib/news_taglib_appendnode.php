d5b070b215e51ac77c3ebe0b75c257d63f197d7d
Christian Achatz <christian.achatz@adventure-php-framework.org>
2012-01-23 19:08:57 +0000

    * Removed <core:addtaglib /> for the <core:appendnode/> tag. * Added further chapters to the UMGT docs. * Removed unnecessary file attribute for controller definitions.

--- apps/sites/apf/pres/taglib/news_taglib_appendnode.php
+++ apps/sites/apf/pres/taglib/news_taglib_appendnode.php
@@ -1,33 +1,30 @@
 <?php
-   import('core::pagecontroller','core_taglib_appendnode');
+/**
+ * @package sites::apf::pres::taglib
+ * @class news_taglib_appendnode
+ *
+ * Enhances the <em>&lt;core:appendnode /&gt;</em> taglib to be able
+ * to include language dependent template fragments.
+ *
+ * @author Christian Achatz
+ * @version
+ * Version 0.1, 29.01.2010<br />
+ */
+class news_taglib_appendnode extends core_taglib_appendnode {
+
+   public function __construct() {
+      parent::__construct();
+   }
 
    /**
-    * @package sites::apf::pres::taglib
-    * @class news_taglib_appendnode
-    *
-    * Enhances the <em>&lt;core:appendnode /&gt;</em> taglib to be able
-    * to include language dependent template fragments.
-    *
-    * @author Christian Achatz
-    * @version
-    * Version 0.1, 29.01.2010<br />
+    * Includes the news template depending on the desired language.
     */
-   class news_taglib_appendnode extends core_taglib_appendnode {
-
-      public function __construct(){
-         parent::__construct();
-      }
-
-      /**
-       * Includes the news template depending on the desired language.
-       */
-      public function onParseTime(){
-         $this->setAttribute('namespace','sites::apf::pres::news');
-         $this->setAttribute('template',$this->__Language.'_news');
-         parent::onParseTime();
-       // end function
-      }
-      
-    // end class
+   public function onParseTime() {
+      $this->setAttribute('namespace', 'sites::apf::pres::news');
+      $this->setAttribute('template', $this->getLanguage() . '_news');
+      parent::onParseTime();
    }
+
+}
+
 ?>
