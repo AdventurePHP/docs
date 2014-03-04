09a54745008140a2a8ace15f2c43339eba9c9f67
Christian Achatz <christian.achatz@adventure-php-framework.org>
2014-03-04 18:46:18 +0000

    Small code optimization.

--- APF/sites/apf/pres/controller/external/FaqController.php
+++ APF/sites/apf/pres/controller/external/FaqController.php
@@ -9,29 +9,20 @@ use APF\core\registry\Registry;
  * @package APF\sites\apf\pres\controller\external
  * @class FaqController
  *
- *  Implements the document controller for the FAQ list.
+ * Implements the document controller for the FAQ list.
  *
  * @author Christian Achatz
  * @version
- *  Version 0.1, 10.01.2009<br />
+ * Version 0.1, 10.01.2009<br />
  */
 class FaqController extends BaseDocumentController {
 
-   /**
-    * @public
-    *
-    *  Displays the FAQ list on the FAQ content page.
-    *
-    * @author Christian Achatz
-    * @version
-    *  Version 0.1, 10.01.2009<br />
-    */
    public function transformContent() {
 
       // get forum database connection
       /* @var $cM ConnectionManager */
-      $cM = &$this->getServiceObject('APF\core\database\ConnectionManager');
-      $SQLForum = &$cM->getConnection('Forum');
+      $cM = & $this->getServiceObject('APF\core\database\ConnectionManager');
+      $SQLForum = & $cM->getConnection('Forum');
 
       // get configuration from the registry
       $forumBaseURL = Registry::retrieve('APF\sites\apf', 'ForumBaseURL');
@@ -47,16 +38,14 @@ class FaqController extends BaseDocumentController {
                        ORDER BY topic_last_post_time DESC;';
       $result = $SQLForum->executeTextStatement($select);
 
-      // get template and prefill it
-      $templatePostsForum = &$this->getTemplate('PostsForum');
-      $templateAuthorLabel = &$this->getTemplate('Author_' . $this->language);
-      $templateCreationDateLabel = &$this->getTemplate('CreationDate_' . $this->language);
+      // get template and pre-fill it
+      $templatePostsForum = & $this->getTemplate('PostsForum');
+      $templateAuthorLabel = & $this->getTemplate('Author_' . $this->language);
+      $templateCreationDateLabel = & $this->getTemplate('CreationDate_' . $this->language);
       $templatePostsForum->setPlaceHolder('AuthorLabel', $templateAuthorLabel->transformTemplate());
       $templatePostsForum->setPlaceHolder('CreationDateLabel', $templateCreationDateLabel->transformTemplate());
 
       // create post list
-      $buffer = (string)'';
-
       while ($data = $SQLForum->fetchData($result)) {
 
          // fill template
@@ -68,12 +57,9 @@ class FaqController extends BaseDocumentController {
          $templatePostsForum->setPlaceHolder('Author', utf8_encode($data['topic_first_poster_name']));
 
          // add current post to list
-         $buffer .= $templatePostsForum->transformTemplate();
+         $this->setPlaceHolder('Entries', $templatePostsForum->transformTemplate(), true);
 
       }
-
-      // add post list to content
-      $this->setPlaceHolder('Entries', $buffer);
 
    }
 
