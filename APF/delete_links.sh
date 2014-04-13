70dc68e6bbb513d6de416d267c937199381b4bb7
Christian Achatz <christian.achatz@adventure-php-framework.org>
2014-04-13 16:47:50 +0200

    Added LINUX symlink scripts

--- APF/delete_links.sh
+++ APF/delete_links.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+####################################################################################################################
+# Automatic deletion of symlinks to the repository clone of the APF code base locally clone'd from                 #
+# https://github.com/AdventurePHP/code.                                                                            #
+####################################################################################################################
+
+rm core
+rm tools
+rm modules
+rm extensions
+rm tests
+rm migration
+
+echo "SUCCESS: Links deleted!"
+exit 0
+
