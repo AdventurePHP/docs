bc741cbc096f935772ba5dd767ceb3ea4dc95568
Christian Achatz <christian.achatz@adventure-php-framework.org>
2015-04-18 15:47:44 +0200

    Corrected site map creation and updated site map content.

--- www/sitemap.php
+++ www/sitemap.php
@@ -1,4 +1,23 @@
 <?php
+use APF\core\configuration\ConfigurationManager;
+use APF\core\configuration\provider\ini\IniConfigurationProvider;
+use APF\core\loader\RootClassLoader;
+use APF\core\loader\StandardClassLoader;
+
 date_default_timezone_set('Europe/Berlin');
-include_once('../APF/core/bootstrap.php');
+
+// pre-define the root path of the root class loader (if necessary)
+$dir = dirname(dirname($_SERVER['SCRIPT_FILENAME']));
+$apfClassLoaderRootPath = $dir . '/APF';
+$apfClassLoaderConfigurationRootPath = $dir . '/config/APF';
+include('../APF/core/bootstrap.php');
+
+// Define class loader for documentation page resources
+RootClassLoader::addLoader(new StandardClassLoader('DOCS', $dir . '/DOCS', $dir . '/config/DOCS'));
+
+/* @var $iniProvider IniConfigurationProvider */
+$iniProvider = ConfigurationManager::retrieveProvider('ini');
+$iniProvider->setOmitConfigSubFolder(true);
+$iniProvider->setOmitContext(true);
+
 include('../DOCS/data/sitemap/sitemap.php');
