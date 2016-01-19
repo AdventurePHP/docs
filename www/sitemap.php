686bcecb2e412509bbbc87ec68af7ccf18206351
Christian Achatz <christian.achatz@adventure-php-framework.org>
2016-01-19 22:26:00 +0100

    Corrected sitemap generation script.

--- www/sitemap.php
+++ www/sitemap.php
@@ -3,6 +3,9 @@ use APF\core\configuration\ConfigurationManager;
 use APF\core\configuration\provider\ini\IniConfigurationProvider;
 use APF\core\loader\RootClassLoader;
 use APF\core\loader\StandardClassLoader;
+use APF\core\logging\Logger;
+use APF\core\registry\Registry;
+use APF\core\singleton\Singleton;
 
 date_default_timezone_set('Europe/Berlin');
 
@@ -14,6 +17,18 @@ include('../APF/core/bootstrap.php');
 
 // Define class loader for documentation page resources
 RootClassLoader::addLoader(new StandardClassLoader('DOCS', $dir . '/DOCS', $dir . '/config/DOCS'));
+
+/* @var $l Logger */
+$l = &Singleton::getInstance(Logger::class);
+$l->setLogThreshold(Logger::$LOGGER_THRESHOLD_ALL);
+
+// configure logger for database debug messages
+$defaultWriter = $l->getLogWriter(
+      Registry::retrieve('APF\core', 'InternalLogTarget')
+);
+$l->addLogWriter('mysqlx', clone $defaultWriter);
+$l->addLogWriter('mysqli', clone $defaultWriter);
+$l->addLogWriter('searchlog', clone $defaultWriter);
 
 /* @var $iniProvider IniConfigurationProvider */
 $iniProvider = ConfigurationManager::retrieveProvider('ini');
