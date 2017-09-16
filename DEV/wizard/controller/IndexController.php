c9ec4d5391e7f763aed27c2cc01fb2e80cd5bac9
Christian Achatz <christian.achatz@adventure-php-framework.org>
2017-09-16 14:46:28 +0200

    Updated index.php adaption step in setup wizard.

--- DEV/wizard/controller/IndexController.php
+++ DEV/wizard/controller/IndexController.php
@@ -7,53 +7,32 @@ class IndexController extends BaseDocumentController {
 
    public function transformContent() {
 
-      $form = & $this->getForm('index-adaption');
+      $form = &$this->getForm('index-adaption');
 
       if ($form->isSent() && $form->isValid()) {
 
-         $control = & $form->getFormElementByName('rel-folder');
-         $folder = $control->getValue();
-
-         $fileName = 'index.php';
+         $fileName = './www/index.php';
          $content = file_get_contents($fileName);
 
          // 1) Disable HttpCacheManager::sendHtmlCacheHeaders();
          $content = preg_replace(
-            '/^HttpCacheManager::sendHtmlCacheHeaders\(\);$/m',
-            '//HttpCacheManager::sendHtmlCacheHeaders();',
-            $content
+               '/^HttpCacheManager::sendHtmlCacheHeaders\(\);$/m',
+               '//HttpCacheManager::sendHtmlCacheHeaders();',
+               $content
          );
 
          // 2) Disable GlobalErrorHandler::registerErrorHandler(new LiveErrorHandler());
          $content = preg_replace(
-            '/^GlobalErrorHandler::registerErrorHandler\(new LiveErrorHandler\(\)\);$/m',
-            '//GlobalErrorHandler::registerErrorHandler(new LiveErrorHandler());',
-            $content
+               '/^GlobalErrorHandler::registerErrorHandler\(new LiveErrorHandler\(\)\);$/m',
+               '//GlobalErrorHandler::registerErrorHandler(new LiveErrorHandler());',
+               $content
          );
 
          // 3) Disable GlobalExceptionHandler::registerExceptionHandler(new LiveExceptionHandler());
          $content = preg_replace(
-            '/^GlobalExceptionHandler::registerExceptionHandler\(new LiveExceptionHandler\(\)\);$/m',
-            '//GlobalExceptionHandler::registerExceptionHandler(new LiveExceptionHandler());',
-            $content
-         );
-
-         // 4) Create release folder stubs
-         $docsFolder = $folder . DIRECTORY_SEPARATOR . '2.X/docs';
-         if (!file_exists($docsFolder)) {
-            mkdir($docsFolder, 0777, true);
-         }
-
-         $downloadsFolder = $folder . DIRECTORY_SEPARATOR . '2.X/downloads';
-         if (!file_exists($downloadsFolder)) {
-            mkdir($downloadsFolder, 0777, true);
-         }
-
-         // 5) Adapt Registry::register('DOCS', 'Releases.LocalDir', '../Build/RELEASES');
-         $content = preg_replace(
-            '/^Registry::register\(\'DOCS\', \'Releases\.LocalDir\', \'(.+)\'\);$/m',
-               'Registry::register(\'DOCS\', \'Releases.LocalDir\', \'' . $folder . '\');',
-            $content
+               '/^GlobalExceptionHandler::registerExceptionHandler\(new LiveExceptionHandler\(\)\);$/m',
+               '//GlobalExceptionHandler::registerExceptionHandler(new LiveExceptionHandler());',
+               $content
          );
 
          file_put_contents($fileName, $content);
@@ -64,4 +43,4 @@ class IndexController extends BaseDocumentController {
       }
    }
 
-} 
+}
